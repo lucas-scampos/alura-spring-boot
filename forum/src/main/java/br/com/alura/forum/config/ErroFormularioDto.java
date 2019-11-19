79cef8630ffdc5d22f605e896a1c39d8caccdcab
lucas.campos <lucas.scampos92@gmail.com>
2019-11-19 07:36:54 -0300

    curso alura springboot

---
+++ forum/src/main/java/br/com/alura/forum/config/ErroFormularioDto.java
@@ -0,0 +1,29 @@
+package br.com.alura.forum.config;
+
+public class ErroFormularioDto {
+	
+	private String campo;
+	private String erro;
+	
+	public ErroFormularioDto(String campo, String erro) {
+		this.campo = campo;
+		this.erro = erro;
+	}
+	
+	public String getCampo() {
+		return campo;
+	}
+	
+	public void setCampo(String campo) {
+		this.campo = campo;
+	}
+	
+	public String getErro() {
+		return erro;
+	}
+	
+	public void setErro(String erro) {
+		this.erro = erro;
+	}
+	
+}
---
+++ forum/src/main/java/br/com/alura/forum/controller/dto/TopicoDto.java
@@ -0,0 +1,57 @@
+package br.com.alura.forum.controller.dto;
+
+import br.com.alura.forum.model.Topico;
+import org.springframework.data.domain.Page;
+import java.time.LocalDateTime;
+
+public class TopicoDto {
+	
+	private Long id;
+	private String titulo;
+	private String mensagem;
+	private LocalDateTime data;
+	
+	public TopicoDto(Topico topico) {
+		this.id = topico.getId();
+		this.titulo = topico.getTitulo();
+		this.mensagem = topico.getMensagem();
+		this.data = topico.getDataCriacao();
+	}
+	
+	public static Page<TopicoDto> converter(Page<Topico> topicos) {
+		return topicos.map(TopicoDto::new);
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	public String getMensagem() {
+		return mensagem;
+	}
+	
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	
+	public LocalDateTime getData() {
+		return data;
+	}
+	
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+	
+}
